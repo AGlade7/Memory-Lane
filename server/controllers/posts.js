8087573c6d471d8be39e8f086916fe73f8e56ffa
Aryaman Gupta <aryamang2002@gmail.com>
2023-06-12 01:01:14 +0530

    Post creation started

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,7 +13,7 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    const podt = req.body;
+    const post = req.body;
     const newPost = new PostMessage(post);
     try {
         await newPost.save();

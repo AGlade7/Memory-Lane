619e5db5a86ef71f1cbb923d2e4768d79ec0baae
Aryaman Gupta <aryamang2002@gmail.com>
2023-06-18 22:52:45 +0530

    Touch

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
 import { useSelector } from "react-redux";
-
 import useStyles from './styles.js';
 import { useDispatch } from 'react-redux';
 import { createPost, updatePost } from '../../actions/posts.js'
